6f532aafc388b5ed0352d9c7fe1a50021451511a
BitWin-dot <hibitwinmail@gmail.com>
2026-03-17 19:45:50 +0100

    Update ema.py

--- indicators/ema.py
+++ indicators/ema.py
@@ -1,1 +1,17 @@
-# EMA indicator calculation
+# indicators/ema.py
+
+class EMA:
+    @staticmethod
+    def calculate(candles, period=50):
+        if len(candles) < period:
+            return candles[-1]['close']
+
+        multiplier = 2 / (period + 1)
+
+        ema = candles[-period]['close']
+
+        for c in candles[-period + 1:]:
+            price = c['close']
+            ema = (price - ema) * multiplier + ema
+
+        return ema
