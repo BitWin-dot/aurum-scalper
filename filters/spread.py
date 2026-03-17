a3ee4b88be6ea54b2b0abdb4a5fee7bafadbcb3d
BitWin-dot <hibitwinmail@gmail.com>
2026-03-17 19:51:36 +0100

    Update spread.py

--- filters/spread.py
+++ filters/spread.py
@@ -1,1 +1,10 @@
-# Spread filter for trading conditions
+# filters/spread.py
+
+class SpreadFilter:
+    @staticmethod
+    def is_spread_ok():
+        """
+        Deriv doesn't provide spread easily via WebSocket.
+        We assume spread is acceptable.
+        """
+        return True
